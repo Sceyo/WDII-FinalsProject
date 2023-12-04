61b7c9f4a3cddcec9aaa3176a395fd7cbe712482
Justine Gonzalez <gonzalezjustine2498@gmail.com>
2023-12-04 19:44:32 +0800

    edited code

--- finals-project/src/App.js
+++ finals-project/src/App.js
@@ -2,20 +2,21 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Components/Home'; 
-import Services from './Components/Services'; 
-import About from './Components/About'; 
-import Contact from './Components/Contact Us';
-import Reviews from './Components/Reviews'; 
-import Login from './Components/Login';
-import SignUp from './Components/Sign Up';
+import Home from './Pages/Home'; 
+import Services from './Pages/Services'; 
+import About from './Pages/About'; 
+import Contact from './Pages/Contact Us';
+import Reviews from './Pages/Reviews'; 
+import Login from './Pages/Login';
+import SignUp from './Pages/Sign Up';
 
 function App() {
   return (
     <>
     <Router>
-      <Navbar />
-      <Home />
+      {/* <Navbar />
+      <Home /> */}
+      <Login />
       <Routes>
         <Route path="/" exact component={Home} />
         <Route path="/services" component={Services} />
