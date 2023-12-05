2886a56d0c3f2458e0f0abf10edf26e2578fdb28
Sceyo <aliserfrancis@gmail.com>
2023-12-05 22:13:46 +0800

    finished about

--- finals-project/src/Components/AboutBody.jsx
+++ finals-project/src/Components/AboutBody.jsx
@@ -10,12 +10,12 @@ export default function AboutBody(){
 
 <div class="row featurette">
   <div class="col-md-7">
-    <h2 class="featurette-heading fw-normal lh-1" style={{fontSize: '50px'}}>About Us</h2>
+    <h2 class="featurette-heading fw-normal lh-1" style={{fontSize: '50px', color: 'black'}}>About Us</h2>
     <p class="lead">Welcome to Smart Delivery – Your Trusted Laundry Delivery Service!</p>
-    <p class="lead2">At Smart Delivery, we understand the importance of time and the value of convenience in today's fast-paced world. That's why we've dedicated ourselves to providing you with a seamless and reliable laundry delivery service that takes the hassle out of your laundry routine.</p>
+    <p class="lead2" style={{width: '90%'}}>At Smart Delivery, we understand the importance of time and the value of convenience in today's fast-paced world. That's why we've dedicated ourselves to providing you with a seamless and reliable laundry delivery service that takes the hassle out of your laundry routine.</p>
   </div>
   <div class="col-md-5">
-  <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src={AU2} alt="500x500 Image" style = {{marginTop: '25px', height: '500px',width:'1200px'}}/>
+  <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" src={AU2} alt="500x500 Image" style = {{marginTop: '50px', height: 'auto',width:'1200px'}}/>
 
   </div>
 </div>
